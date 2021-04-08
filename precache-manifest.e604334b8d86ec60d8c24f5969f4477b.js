2d2fa0b216f6529660a4697ad58de392a50d9779
Balaji Mohan <smbalaji89@gmail.com>
2021-04-08 17:07:30 +0530

    Updates

---
+++ precache-manifest.e604334b8d86ec60d8c24f5969f4477b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "860337db48869e81c12b",
+    "url": "/meynikara_exchange/static/css/main.1052e9e5.chunk.css"
+  },
+  {
+    "revision": "860337db48869e81c12b",
+    "url": "/meynikara_exchange/static/js/main.860337db.chunk.js"
+  },
+  {
+    "revision": "3ebf84af629a1d0a7339",
+    "url": "/meynikara_exchange/static/css/1.2b87c278.chunk.css"
+  },
+  {
+    "revision": "3ebf84af629a1d0a7339",
+    "url": "/meynikara_exchange/static/js/1.3ebf84af.chunk.js"
+  },
+  {
+    "revision": "5be5a0cc011c3a9d3668",
+    "url": "/meynikara_exchange/static/js/runtime~main.5be5a0cc.js"
+  },
+  {
+    "revision": "b248dfb24924aeed485dd74fa8e274b8",
+    "url": "/meynikara_exchange/index.html"
+  }
+];
